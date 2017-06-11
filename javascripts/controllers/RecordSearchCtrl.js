2e5f5cc02d552698f613459e4235c8d217bbb6e0
Isabel Ramos <iramos1226@gmail.com>
2017-06-11 14:05:47 -0500

    console logging artist name and title

--- javascripts/controllers/RecordSearchCtrl.js
+++ javascripts/controllers/RecordSearchCtrl.js
@@ -11,7 +11,7 @@ app.controller("RecordSearchCtrl", function($rootScope, $scope, RecordFactory) {
     let getSearchedRecords = () => {
         RecordFactory.getSearchedRecordsList($scope.userRecordSearch).then((searchedRecordz) => {
             $scope.searchedRecords = searchedRecordz;
-            console.log("searchedRecords", $scope.searchedRecords);
+            // console.log("searchedRecords", $scope.searchedRecords);
         }).catch((error) => {
             console.log("error", error);
         });
@@ -20,7 +20,15 @@ app.controller("RecordSearchCtrl", function($rootScope, $scope, RecordFactory) {
     let getAlbumArt = () => {
         RecordFactory.getSearchedRecordArt($scope.userRecordSearch).then((searchedRecordArtz) => {
             $scope.searchedRecordArts = searchedRecordArtz;
-            console.log("searchedRecordArts", $scope.searchedRecordArts);
+            // console.log("searchedRecordArts", $scope.searchedRecordArts);
+            $scope.albumArtObject = searchedRecordArtz.data.results.albummatches.album;
+            // console.log("$scope.albumArtObject", $scope.albumArtObject);
+            angular.forEach($scope.albumArtObject,function(value,index){
+                $scope.searchedArtistName = value.artist;
+                $scope.searchedArtistAlbumTitle = value.name;
+                console.log("$scope.searchedArtistName", $scope.searchedArtistName);
+                console.log("$scope.searchedArtistAlbumTitle", $scope.searchedArtistAlbumTitle);
+            });
         }).catch((error) => {
             console.log("error", error);
         });
