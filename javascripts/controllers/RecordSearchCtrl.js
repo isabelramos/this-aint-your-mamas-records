8aff5666b53236111acdadff9894a6a090898183
Isabel Ramos <iramos1226@gmail.com>
2017-06-11 20:55:37 -0500

    trying to get images from api

--- javascripts/controllers/RecordSearchCtrl.js
+++ javascripts/controllers/RecordSearchCtrl.js
@@ -2,9 +2,8 @@ app.controller("RecordSearchCtrl", function($rootScope, $scope, RecordFactory) {
 	$scope.userRecordSearch = "";
     $scope.searchedRecords = [];
     $scope.searchedRecordArts = [];
-    $scope.searchedArtistNameArray = [];
-    $scope.searchedArtistAlbumTitleArray = [];
-    // $scope.searchedArtistAndTitleArray = [];
+    // $scope.searchedArtistNameArray = [];
+    // $scope.searchedArtistAlbumTitleArray = [];
 
 
     $scope.searchRecordsButton = () => {
@@ -23,7 +22,7 @@ app.controller("RecordSearchCtrl", function($rootScope, $scope, RecordFactory) {
 
     let getAlbumArt = () => {
         $scope.searchedArtistAndTitleArray = [];
-        
+
         RecordFactory.getSearchedRecordArt($scope.userRecordSearch).then((searchedRecordArtz) => {
             $scope.searchedRecordArts = searchedRecordArtz;
             // console.log("searchedRecordArts", $scope.searchedRecordArts);
@@ -32,9 +31,18 @@ app.controller("RecordSearchCtrl", function($rootScope, $scope, RecordFactory) {
             angular.forEach($scope.albumArtObject,function(value,index){
                 $scope.searchedArtistName = value.artist;
                 $scope.searchedArtistAlbumTitle = value.name;
+                $scope.searchedAlbumArt = value.image;
+                // console.log("$scope.searchedAlbumArt", $scope.searchedAlbumArt);
                 // $scope.searchedArtistNameArray.push($scope.searchedArtistName);
                 // $scope.searchedArtistAlbumTitleArray.push($scope.searchedArtistAlbumTitle);
                 $scope.searchedArtistAndTitleArray.push($scope.searchedArtistName + " " + $scope.searchedArtistAlbumTitle);
+                $scope.albumArtObject = searchedRecordArtz.data.results.albummatches.album;
+                angular.forEach($scope.searchedAlbumArt,function(value,index){
+                    console.log("$scope.searchedAlbumArt", $scope.searchedAlbumArt);
+                    // $scope.albumArt = value.#text;
+                    $scope.albumArt = value.size;
+                    console.log("$scope.albumArt", $scope.albumArt);
+                });
             });
         }).catch((error) => {
             console.log("error", error);
