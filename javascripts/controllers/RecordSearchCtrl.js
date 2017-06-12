ad26ead0bca7e02fbb863ca15629ead7d4d817aa
Isabel Ramos <iramos1226@gmail.com>
2017-06-12 18:57:08 -0500

    successfully displaying album info to dom

--- javascripts/controllers/RecordSearchCtrl.js
+++ javascripts/controllers/RecordSearchCtrl.js
@@ -1,48 +1,25 @@
 app.controller("RecordSearchCtrl", function($rootScope, $scope, RecordFactory) {
 	$scope.userRecordSearch = "";
-    $scope.searchedRecords = [];
     $scope.searchedRecordArts = [];
-    // $scope.searchedArtistNameArray = [];
-    // $scope.searchedArtistAlbumTitleArray = [];
 
 
     $scope.searchRecordsButton = () => {
-        getSearchedRecords();
-        getAlbumArt();
+        getAlbumInfo();
     };
 
-    let getSearchedRecords = () => {
-        RecordFactory.getSearchedRecordsList($scope.userRecordSearch).then((searchedRecordz) => {
-            $scope.searchedRecords = searchedRecordz;
-            // console.log("searchedRecords", $scope.searchedRecords);
-        }).catch((error) => {
-            console.log("error", error);
-        });
-    };
 
-    let getAlbumArt = () => {
+    let getAlbumInfo = () => {
         $scope.searchedArtistAndTitleArray = [];
 
         RecordFactory.getSearchedRecordArt($scope.userRecordSearch).then((searchedRecordArtz) => {
-            $scope.searchedRecordArts = searchedRecordArtz;
-            // console.log("searchedRecordArts", $scope.searchedRecordArts);
-            $scope.albumArtObject = searchedRecordArtz.data.results.albummatches.album;
-            // console.log("$scope.albumArtObject", $scope.albumArtObject);
-            angular.forEach($scope.albumArtObject,function(value,index){
-                $scope.searchedArtistName = value.artist;
-                $scope.searchedArtistAlbumTitle = value.name;
-                $scope.searchedAlbumArt = value.image;
-                // console.log("$scope.searchedAlbumArt", $scope.searchedAlbumArt);
-                // $scope.searchedArtistNameArray.push($scope.searchedArtistName);
-                // $scope.searchedArtistAlbumTitleArray.push($scope.searchedArtistAlbumTitle);
-                $scope.searchedArtistAndTitleArray.push($scope.searchedArtistName + " " + $scope.searchedArtistAlbumTitle);
-                $scope.albumArtObject = searchedRecordArtz.data.results.albummatches.album;
-                angular.forEach($scope.searchedAlbumArt,function(value,index){
-                    console.log("$scope.searchedAlbumArt", $scope.searchedAlbumArt);
-                    // $scope.albumArt = value.#text;
-                    $scope.albumArt = value.size;
-                    console.log("$scope.albumArt", $scope.albumArt);
-                });
+            let searchedRecordArts = searchedRecordArtz;
+            let albumArtObject = searchedRecordArtz.data.results.albummatches.album;
+            angular.forEach(albumArtObject,function(value,index){
+                let singleAlbumInfo = {};
+                singleAlbumInfo.searchedArtistName = value.artist;
+                singleAlbumInfo.searchedArtistAlbumTitle = value.name;
+                singleAlbumInfo.searchedAlbumArt = value.image[3]["#text"];
+                $scope.searchedArtistAndTitleArray.push(singleAlbumInfo);
             });
         }).catch((error) => {
             console.log("error", error);
