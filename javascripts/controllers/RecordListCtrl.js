7ba30fd02f2d565b087e9f6b34720466d5dc1254
Isabel Ramos <iramos1226@gmail.com>
2017-06-12 20:32:47 -0500

    nav bar and user info are printing to dom

---
+++ javascripts/controllers/RecordListCtrl.js
@@ -0,0 +1,25 @@
+app.controller("RecordListCtrl", function($rootScope, $scope, RecordFactory) {
+	// $scope.addresses = [];
+
+	// let getAddresses = () => {
+ //    	AddressFactory.getAddressList($rootScope.user.uid).then((addressez) => {
+ //    		$scope.addresses = addressez;
+ //            console.log(addressez);
+ //    	}).catch((error) => {
+ //      		console.log("error", error);
+ //    	});
+ //  	};
+
+	// getAddresses();
+
+
+ //    $scope.deleteAddress = (id) => {
+ //    	AddressFactory.deletz(id).then(() => {
+ //    		getAddresses();
+ //    	}).catch((error) => {
+ //    		console.log("deleteAddress error", error);
+ //    	});
+ //    };
+
+
+});
