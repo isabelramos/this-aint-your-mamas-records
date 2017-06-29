4d1c8f8abe52a93cfd2b54b2c05b9a35567e6a4b
Isabel Ramos <iramos1226@gmail.com>
2017-06-28 23:21:50 -0500

    fixed bug

--- javascripts/controllers/ViewFriendsCtrl.js
+++ javascripts/controllers/ViewFriendsCtrl.js
@@ -1,10 +1,15 @@
-app.controller("ViewFriendsCtrl", function($routeParams, $scope, UserFactory) {
+app.controller("ViewFriendsCtrl", function($rootScope, $routeParams, $scope, UserFactory) {
+
+  $scope.isFriendProfile = $routeParams.uid;
+  $scope.isCurrentUser = $rootScope.user.uid;
+  console.log("$scope.isCurrentUser", $scope.isCurrentUser);
+
 	$scope.users = [];
 
 	let getFriends = () => {
     	UserFactory.getAllUsers().then((userz) => {
     		$scope.users = userz;
-            console.log(userz);
+            console.log("userz", userz);
     	}).catch((error) => {
       		console.log("error", error);
     	});
